8e2d1aec8bd681cb8a298ce7b41524b92d6261b3
vahidmohsseni <vahidmohsseni@yahoo.com>
2023-01-24 14:53:56 +0000

    Add dead code

--- src/utils.rs
+++ src/utils.rs
@@ -3,12 +3,14 @@ use std::{
     path::{Path, PathBuf}, io::{BufReader, self}
 };
 
-
+#[allow(dead_code)]
 pub(crate) fn file_reader(path: &Path) -> BufReader<File> {
     let file = OpenOptions::new().read(true).open(path).unwrap();
     return BufReader::new(file);
 }
 
+
+#[allow(dead_code)]
 pub(crate) fn scan_dir(dir: &Path) -> io::Result<Vec<PathBuf>> {
     let files = fs::read_dir(&dir)?
     .map(|res| res.map(|e| e.path()))
@@ -16,11 +18,14 @@ pub(crate) fn scan_dir(dir: &Path) -> io::Result<Vec<PathBuf>> {
     Ok(files)
 }
 
+
+#[allow(dead_code)]
 pub(crate) fn remove_dir(dir: &Path) -> io::Result<()> {
     fs::remove_dir_all(dir)?;
     Ok(())
 }
 
+#[allow(dead_code)]
 pub(crate) fn create_dir(dir: &Path) -> io::Result<()> {
     fs::create_dir(dir)?;
     Ok(())
