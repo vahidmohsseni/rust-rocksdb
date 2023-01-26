90163397bf500314e84dc623dd86c07c653d347b
vahidmohsseni <vahidmohsseni@yahoo.com>
2023-01-26 22:49:33 +0000

    Add untested codes

--- src/engine.rs
+++ src/engine.rs
@@ -122,3 +122,118 @@ impl Db {
         return Ok(());
     }
 }
+
+#[cfg(test)]
+mod test {
+    use std::{path::PathBuf, time::SystemTime};
+
+    use rand::Rng;
+
+    use crate::{
+        entry::Entry,
+        storage::Storage,
+        storage_iterator::StorageIterator,
+        utils::{create_dir, remove_dir, scan_dir},
+    };
+
+    use super::Db;
+
+    #[test]
+    fn init_engine() {
+        let mut range = rand::thread_rng();
+        let path = PathBuf::from(format!("./test-{}-temp", range.gen::<u32>()));
+
+        create_dir(&path).unwrap();
+
+        let mut db = Db::new(path);
+
+        let key1 = b"Hello".to_owned();
+        let value1 = *b"World!";
+
+        db.set(&key1, &value1).unwrap();
+
+        assert_eq!(b"Hello".to_owned().to_vec(), db.get(&key1).unwrap().key);
+
+        let key2 = b"Name".to_owned();
+        let value2 = *b"Vahid";
+
+        db.set(&key2, &value2).unwrap();
+
+        assert_eq!(
+            b"Vahid".to_owned().to_vec(),
+            db.get(&key2).unwrap().value.unwrap()
+        );
+
+        db.delete(&key1).unwrap();
+
+        assert_eq!(true, db.get(&key1).unwrap().deleted);
+
+        // Clean up
+        remove_dir(&db.dir).expect("Error: could not remove the directory");
+    }
+
+    #[test]
+    fn read_from_multiple_files() {
+        let mut range = rand::thread_rng();
+        let path = PathBuf::from(format!("./test-{}-temp", range.gen::<u32>()));
+
+        create_dir(&path).unwrap();
+
+        let mut storage = Storage::new(&path).unwrap();
+
+        let key1 = b"Hello".to_owned();
+        let value1 = *b"World!";
+        let timestamp1 = SystemTime::now().elapsed().unwrap().as_micros();
+        storage
+            .set(&key1, &value1, false, timestamp1)
+            .expect("Error: could not write in the file");
+
+        let key2 = b"Name".to_owned();
+        let value2 = *b"Vahid";
+        let timestamp2 = SystemTime::now().elapsed().unwrap().as_micros();
+        storage
+            .set(&key2, &value2, false, timestamp2)
+            .expect("Error: could not write in the file");
+
+        let key3 = b"gg".to_owned();
+        let value3 = *b"wp";
+        let timestamp3 = SystemTime::now().elapsed().unwrap().as_micros();
+        storage
+            .set(&key3, &value3, false, timestamp3)
+            .expect("Error: could not write in the file");
+
+        let key4 = b"Name".to_owned();
+        let timestamp4 = SystemTime::now().elapsed().unwrap().as_micros();
+        storage
+            .delete(&key4, timestamp4)
+            .expect("Error: could not complete delete operation");
+
+        storage.commit().expect("Error: could not flush the file");
+
+        drop(storage);
+
+        let mut storage = Storage::new(&path).unwrap();
+
+        let key1 = b"Hello".to_owned();
+        let value1 = *b"RUST";
+        let timestamp1 = SystemTime::now().elapsed().unwrap().as_micros();
+        storage
+            .set(&key1, &value1, false, timestamp1)
+            .expect("Error: could not write in the file");
+
+        let key2 = b"gg".to_owned();
+        let timestamp2 = SystemTime::now().elapsed().unwrap().as_micros();
+        storage
+            .delete(&key2, timestamp2)
+            .expect("Error: could not write in the file");
+
+        storage.commit().unwrap();
+
+        drop(storage);
+
+        let db = Db::init_from_existing(path);
+        
+        // Clean up
+        // remove_dir(&path).unwrap();
+    }
+}
